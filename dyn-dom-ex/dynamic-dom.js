d2da71924a49e820510e07d12816cd581ec30521
Eduardo Lombana <edlombanal@gmail.com>
2022-05-01 13:45:50 -0500

    Add DOM Hello World Practice

---
+++ dyn-dom-ex/dynamic-dom.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#btn');
+btn.onclick = () => {
+    alert('Hello World');
+};
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click',()=>{
+    alert('Hello World');
+});
