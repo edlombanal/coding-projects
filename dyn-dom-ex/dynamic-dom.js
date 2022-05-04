e07ce337f46d2af701bb9fdd603ba22b2ccb58ac
Eduardo Lombana <edlombanal@gmail.com>
2022-05-03 20:40:42 -0500

    Add Dom color change exercise

--- dyn-dom-ex/dynamic-dom.js
+++ dyn-dom-ex/dynamic-dom.js
@@ -7,3 +7,13 @@ const btn2 = document.querySelector('#btn2');
 btn2.addEventListener('click',()=>{
     alert('Hello World');
 });
+
+function alertF(){
+    alert('Hello World');
+};
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click',function(e){
+    console.log(e.target);
+    e.target.style.backgroundColor = 'red';
+});
